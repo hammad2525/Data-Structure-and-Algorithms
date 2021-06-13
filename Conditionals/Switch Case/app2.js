729bedda8dd1fbcc932f06b4b64340dc94abdbb6
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-13 11:17:14 +0500

    completed second switch case task

---
+++ Conditionals/Switch Case/app2.js
@@ -0,0 +1,33 @@
+// You are given two numbers, num1 = 4 and num2 = 7. 
+// Add both and return true if both are equal to 11.
+//  Otherwise, return false, if they don’t equal 11.
+
+let num1 = 4;
+let num2 = 5;
+
+let sum = num1 + num2;
+
+switch (sum) {
+    case 1:
+
+        console.log('sub of two number is' + sum);
+
+        break;
+
+    case 2:
+
+        console.log('mul of two number is' + sum);
+
+        break;
+
+    case sum:
+
+        console.log('sum of two number is' +" "+ sum);
+
+        break;
+
+
+
+    default:
+        break;
+}
