dcfeb0331688cea92bd6df2042b166b99609cbe5
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-13 09:50:52 +0500

    completed if else fourth task

---
+++ Conditionals/IF-Else/app4.js
@@ -0,0 +1,19 @@
+// Suppose you are given a number and you need to check whether it’s even or odd.
+//  How'd you do that?
+
+let number = 22;
+
+if (number % 2 == 0) {
+
+    console.log('number is even because it doesnot has any remainder');
+}
+
+else if (number % 2 != 0) {
+
+    console.log('number is odd because it has some remainder');
+}
+
+else {
+
+    console.log(' there is no output ');
+}
