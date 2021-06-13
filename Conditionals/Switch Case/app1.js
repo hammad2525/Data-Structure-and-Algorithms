f098810363d793c5b0a9870ff906fae002d86757
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-13 09:20:42 +0500

    completed first switch case task

---
+++ Conditionals/Switch Case/app1.js
@@ -0,0 +1,22 @@
+// Consider the situation in which you are required to check
+//  if a number is 1 or 0. If it’s 1 you need to print true.
+//   Otherwise, you should print false.
+
+let number = 1;
+
+switch (number) {
+    case 1:
+
+        console.log('True');
+        break;
+
+    case 2:
+
+        console.log('False');
+        break;
+
+    default:
+
+        console.log('Np proper result is found');
+        break;
+}
