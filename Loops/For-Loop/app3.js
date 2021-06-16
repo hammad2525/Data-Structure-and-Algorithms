db3c9cc6396994be24dfa8e89f525e57b7d4faba
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-16 12:05:37 +0500

    completed third loop task

---
+++ Loops/For-Loop/app3.js
@@ -0,0 +1,17 @@
+// Work on the problem above to dynamically print the name of each animal
+//  and prepend it with ‘this is’ conditionally.
+//  Print ‘something else’ if the name doesn’t equal any of the names in the list.
+
+let animal = ['cat', 'rabbit','pigeon','parrot','goldfish'];
+
+for (let i = 0; i < animal.length; i++) {
+
+    console.log('this is '+animal[i]);
+    
+
+}
+{
+    
+    console.log('this is something else');
+}
+
