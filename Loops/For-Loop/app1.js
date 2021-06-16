e3ae600bde94843ead7329b32853f64488b525ea
Hammad Khan <hammad.khan5980@gmail.com>
2021-06-16 12:24:22 +0500

    loop first task done

---
+++ Loops/For-Loop/app1.js
@@ -0,0 +1,20 @@
+// Suppose you are given an array: [1, 8, 7, 3, 2, 9, 10, 12].
+//  You need to find out whether a number is prime or not
+//   (Hint: a prime number is a number that is divisible by 1 and itself).
+// What approach would you take to do that? Support it with the relevant code.
+
+let array = [1, 8, 7, 3, 2, 9, 10, 12];
+
+array = array.filter(number) =>{
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+
+        if (number % i === 0)
+            return false;
+        else {
+
+            return true;
+        }
+    };
+
+    console.log(array);
+}
